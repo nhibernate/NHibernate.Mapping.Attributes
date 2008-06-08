9c9c28694fd0ac65fb3be84a40e28cda6441a604
Diego Jancic <jancic@gmail.com>
2008-06-08 04:30:35 +0000

    Added NAnt build scripts for all projects (except Burrow and Validator) Fixed build of NHibernate.Caches.Prevalence Removed old non-sense test for NHibernate.UserTypes.SqlTypes (those ones related to Nullable Types)
    git-svn-id: svn://svn.code.sf.net/p/nhcontrib/code/trunk/src/NHibernate.Mapping.Attributes@411 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.Mapping.Attributes/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Reflection;
+using System.Security;
+
+[assembly: CLSCompliantAttribute(true)]
+[assembly: AssemblyTitleAttribute("NHibernate.Mapping.Attributes")]
+[assembly: AssemblyDescriptionAttribute("Library of attributes which can be used to decorate your entities and generate hbm.xml mapping documents for NHibernate 2.0.0.Alpha1.")]
+[assembly: AssemblyCompanyAttribute("NHibernate.org")]
+[assembly: AssemblyProductAttribute("NHibernate.Mapping.Attributes")]
+[assembly: AssemblyCopyrightAttribute("Licensed under LGPL.")]
+[assembly: AssemblyVersionAttribute("2.0.0.1001")]
+[assembly: AssemblyInformationalVersionAttribute("2.0.0.1001")]
+[assembly: AssemblyFileVersionAttribute("2.0.0.1001")]
+//[assembly: AssemblyKeyFileAttribute("../../src/NHibernate.snk")]
+[assembly: AssemblyDelaySignAttribute(false)]
+[assembly: AllowPartiallyTrustedCallers()]
